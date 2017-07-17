da45d1d08cb48d103f29549a4123c4f788b99c19
Poonam Thawani <pthawani@pthawani006847.starbucks.net>
2017-07-16 18:38:26 -0700

    Updated the host and port

--- src/main/scala/uw/demo/friendlocator/FriendLocatorHttpRoute.scala
+++ src/main/scala/uw/demo/friendlocator/FriendLocatorHttpRoute.scala
@@ -4,12 +4,30 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.{Directives, Route}
+import akka.stream.{ActorMaterializer, Materializer}
+import uw.demo.friendlocator.service.FriendLocatorService
+import akka.http.scaladsl.Http
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.StdIn
+import scala.concurrent.ExecutionContext
 
 /**
   * Created by pthawani on 7/16/17.
   */
-trait FriendLocatorHttpRoute {
+trait FriendLocatorHttpRoute extends Directives{
 
-  val route : Route = ???
+  implicit def system: ActorSystem
+  implicit def materializer: Materializer
+  implicit def ec: ExecutionContext
+  implicit def friendLocatorService: FriendLocatorService
+
+
+  val route =
+    path("hello") {
+      get {
+        complete("New World")
+      }
+    }
 
 }
